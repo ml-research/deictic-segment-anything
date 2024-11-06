a2dc8a73ed25565c6df8f65398e665378e4d70dc
hkrsnd <microapple56@gmail.com>
2024-11-06 15:54:41 +0100

    refactor utils scripts

--- src/sam_utils.py
+++ src/sam_utils.py
@@ -1,84 +1,118 @@
 import torch
 from groundingdino.util import box_ops
-from itsdangerous import NoneAlgorithm
-from visual_genome_utils import objdata_to_box, scene_graph_to_language
+from visual_genome_utils import objdata_to_box
 
 
 def to_xyxy(boxes):
-    """xywh to xyxy format
+    """Convert boxes from xywh format to xyxy format.
 
     Args:
-        boxes (_type_): _description_
+        boxes (torch.Tensor): A tensor of boxes in xywh format.
 
     Returns:
-        _type_: _description_
+        torch.Tensor: A tensor of boxes in xyxy format.
     """
-    xyxy_boxes = []
-    for box in boxes:
-        x = box[0]
-        y = box[1]
-        # x + w
-        x_2 = x + box[2]
-        # y + h
-        y_2 = y + box[3]
-        xyxy_boxes.append(torch.tensor([x, y, x_2, y_2]))
+    xyxy_boxes = [
+        torch.tensor([x, y, x + w, y + h])
+        for x, y, w, h in boxes
+    ]
     return torch.stack(xyxy_boxes)
 
 
 def transform_boxes(boxes, image_source):
-    # box: normalized box xywh -> unnormalized xyxy
-    H, W, _ = image_source.shape
-    # boxes_xyxy = box_ops.box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H])
+    """Transform normalized xywh boxes to unnormalized xyxy boxes.
+
+    Args:
+        boxes (torch.Tensor): A tensor of boxes in xywh format.
+        image_source (numpy.ndarray): The source image to obtain dimensions.
+
+    Returns:
+        torch.Tensor or NoneAlgorithm: Transformed boxes in xyxy format, or NoneAlgorithm on failure.
+    """
     try:
-        boxes_xyxy = to_xyxy(boxes)  # * torch.Tensor([W, H, W, H])
-        return boxes_xyxy
+        return to_xyxy(boxes)
     except RuntimeError:
         return NoneAlgorithm
 
 
 def to_object_ids(target_atoms):
-    object_ids = [int(atom.terms[0].name.split("_")[-1]) for atom in target_atoms]
-    return object_ids
+    """Extract object IDs from FOL target atoms.
+
+    Args:
+        target_atoms (list): List of target atoms.
+
+    Returns:
+        list: A list of object IDs.
+    """
+    return [int(atom.terms[0].name.split("_")[-1]) for atom in target_atoms]
 
 
 def to_boxes(target_atoms, data_index, visual_genome_utils):
-    # get box from relations!! not objects
+    """Extract bounding boxes for target atoms using Visual Genome data.
+
+    Args:
+        target_atoms (list): List of target atoms.
+        data_index (int): Index in the Visual Genome dataset.
+        visual_genome_utils (object): Utilities for accessing Visual Genome data.
+
+    Returns:
+        list: A list of bounding boxes.
+    """
     object_ids = to_object_ids(target_atoms)
     relations = visual_genome_utils.all_relationships[data_index]["relationships"]
+
     boxes = []
-    for id in object_ids:
+    for obj_id in object_ids:
         for rel in relations:
-            if rel["object"]["object_id"] == id:
+            if rel["object"]["object_id"] == obj_id:
                 boxes.append(objdata_to_box(rel["object"]))
                 break
-            elif rel["subject"]["object_id"] == id:
+            elif rel["subject"]["object_id"] == obj_id:
                 boxes.append(objdata_to_box(rel["subject"]))
                 break
     return boxes
 
 
 def to_boxes_with_sgg(target_atoms, image_id, visual_genome_utils):
-    # get box from relations!! not objects
+    """Extract bounding boxes for target atoms using Scene Graph Generation data.
+
+    Args:
+        target_atoms (list): List of target atoms.
+        image_id (int): Image ID for accessing relationships.
+        visual_genome_utils (object): Utilities for accessing Visual Genome data.
+
+    Returns:
+        list: A list of bounding boxes.
+    """
     object_ids = to_object_ids(target_atoms)
     relations = visual_genome_utils.all_relationships[image_id]
+
     boxes = []
-    for id in object_ids:
+    for obj_id in object_ids:
         for rel in relations:
-            if rel["o_unique_id"] == id:
+            if rel["o_unique_id"] == obj_id:
                 boxes.append(rel["obox"])
                 break
-            if rel["s_unique_id"] == id:
+            if rel["s_unique_id"] == obj_id:
                 boxes.append(rel["sbox"])
                 break
     return boxes
 
 
 def to_transformed_boxes(boxes, image_source, sam_predictor, device):
-    # box: normalized box xywh -> unnormalized xyxy
-    H, W, _ = image_source.shape
-    # boxes_xyxy = box_ops.box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H])
+    """Apply transformations to bounding boxes using a SAM predictor.
+
+    Args:
+        boxes (list): List of bounding boxes.
+        image_source (numpy.ndarray): The source image.
+        sam_predictor (object): SAM predictor object with transform capabilities.
+        device (torch.device): The device to convert the boxes to.
+
+    Returns:
+        torch.Tensor: A tensor of transformed boxes.
+    """
     try:
-        boxes_xyxy = to_xyxy(boxes)  # * torch.Tensor([W, H, W, H])
+        boxes_xyxy = to_xyxy(boxes)
         transformed_boxes = sam_predictor.transform.apply_boxes_torch(
             boxes_xyxy, image_source.shape[:2]
         ).to(device)
